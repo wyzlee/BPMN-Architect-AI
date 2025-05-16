e59bd2a51cae396e9e1e32188f41fe960a9d2e62
Wyzlee - Business made Wisely <opodio@wyzlee.com>
2025-05-16 13:29:04 +0200

    Add next.config.js with standalone output for Render deployment

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // Ajout de la configuration pour le déploiement sur Render
+  output: 'standalone',
+};
+
+module.exports = nextConfig;
